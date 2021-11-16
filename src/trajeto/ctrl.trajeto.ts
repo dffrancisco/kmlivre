12a07e2fc91f73a05161eba5fe2393247b930e5c
Francisco Alves <dffrancisco@gmail.com>
2021-11-16 10:24:18 -0300

    fiz date hora, moment

--- src/trajeto/ctrl.trajeto.ts
+++ src/trajeto/ctrl.trajeto.ts
@@ -5,16 +5,31 @@ import { iUsuario } from '../models/interfaces'
 import util from "../services/util";
 import moment from 'moment'
 
-const nDate = new Date().toLocaleString('bt-BR', { timeZone: 'America/Sao_Paulo' });
+// const nDate = new Date().toLocaleString('bt-BR', { timeZone: 'America/Sao_Paulo' });
 moment.locale('pt-br');
 
 // moment().tz("America/Sao_Paulo").format();
 
-console.log('scp', nDate);
+// console.log('scp', nDate);
 
-console.log('mo-ndate', moment().format(nDate))
-console.log('mo', moment().format())
-console.log('HORA', moment().format('LTS'))
+// console.log('mo-ndate', moment().format(nDate))
+// console.log('mo', moment(nDate, 'YYYY-MM-DD HH:mm:ss').format('HH:mm:ss'))
+// console.log('HORA', moment().format('LTS'))
+
+const helper = {
+  getHora() {
+    const nDate = new Date().toLocaleString('bt-BR', { timeZone: 'America/Sao_Paulo' });
+    return moment(nDate, 'YYYY-MM-DD HH:mm:ss').format('HH:mm:ss')
+  },
+  getDate() {
+    const nDate = new Date().toLocaleString('bt-BR', { timeZone: 'America/Sao_Paulo' });
+    return moment(nDate, 'YYYY-MM-DD HH:mm:ss').format('YYYY-MM-DD')
+  }
+}
+
+
+console.log(helper.getHora(), 'hora');
+console.log(helper.getDate(), 'data');
 
 const controllers = {
   usuario: <iUsuario>{},
@@ -31,8 +46,11 @@ const controllers = {
 
     let { param } = req.body
 
-    param.data = moment().format('YYYY-MM-DD')
-    param.hora = moment().format('LTS')
+    // param.data = moment().format('YYYY-MM-DD')
+    // param.hora = moment().format('LTS')
+    param.data = helper.getDate()
+    param.hora = helper.getHora()
+
     param.id_user = controllers.usuario.id_user
 
 
@@ -48,8 +66,10 @@ const controllers = {
   async finalizarTrajeto(req: Request) {
     let { param } = req.body
 
-    param.data = moment().format('YYYY-MM-DD')
-    param.hora = moment().format('LTS')
+    // param.data = moment().format('YYYY-MM-DD')
+    // param.hora = moment().format('LTS')
+    param.data = helper.getDate()
+    param.hora = helper.getHora()
     param.id_user = controllers.usuario.id_user
 
     try {
