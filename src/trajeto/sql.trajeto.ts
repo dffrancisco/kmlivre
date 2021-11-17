d74bf63b6dd9f12c7b75412d5033d3d62f58fe30
dffrancisco <dffrancisco@gmail.com>
2021-11-16 21:14:56 -0300

    up

--- src/trajeto/sql.trajeto.ts
+++ src/trajeto/sql.trajeto.ts
@@ -3,11 +3,13 @@ import mysql from "../db/mysqlConnect";
 
 export default {
 
-  getTrajetos: (param: object) => {
-    let sql = `SELECT id_router, id_user, b_km, DATE_FORMAT(b_data, '%Y-%m-%d') b_data, b_hora, b_latitude, 
-                b_longitude, f_km, DATE_FORMAT(f_data, '%Y-%m-%d') f_data, f_hora, f_latitude, f_longitude 
+  getRotas: (param: object) => {
+    let sql = `SELECT id_router, b_km, DATE_FORMAT(b_data, '%Y-%m-%d') b_data, b_hora, b_latitude, 
+                b_longitude, f_km, DATE_FORMAT(f_data, '%Y-%m-%d') f_data, f_hora, f_latitude, 
+                f_longitude, (f_km - b_km) distancia 
                 FROM km_router
-                WHERE id_user = :id_user`;
+                WHERE id_user = :id_user
+                order by b_data desc`;
     sql = prepare.prepareSQL(sql, param);
 
     return mysql.query(sql);
@@ -42,12 +44,24 @@ export default {
                   f_hora= :hora,
                   f_latitude = :latitude,
                   f_longitude = :longitude		
-              WHERE id_user = :id_user`
+              WHERE id_router = :id_router
+              and id_user = :id_user`
 
     sql = prepare.prepareSQL(sql, param);
 
     return mysql.query(sql);
-  }
+  },
 
+  getUltimoTrajeto(param: object) {
+    let sql = `SELECT f_km from km_router
+                WHERE id_user = :id_user
+                AND f_data IS NOT null
+                ORDER BY id_router DESC
+                LIMIT 1`
+
+    sql = prepare.prepareSQL(sql, param);
+
+    return mysql.query(sql);
+  },
 
 };
